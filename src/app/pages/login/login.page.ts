f1ceb251df7350de87005293b334f49a3de8f2fe
DSLeonardoPavez <leonardo.pavez.06@gmail.com>
2023-11-15 20:09:51 -0300

    Arregle errores varios

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -3,7 +3,6 @@ import { GetOptions, Storage } from '@capacitor/storage';
 import { user } from '../../modules/user';
 import { Router } from '@angular/router';
 
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -11,13 +10,13 @@ import { Router } from '@angular/router';
 })
 export class LoginPage {
   password: string;
-  router: any;
-  
+  router: Router;
+
   constructor(router: Router) {
     this.router = router;
     this.password = '';
 
-    this.user = {
+    this.currentUser = {
       user: '',
       name: '',
       lastName: '',
@@ -26,38 +25,46 @@ export class LoginPage {
     };
   }
 
-  user: user;
+  currentUser: user;
   loggedIn: boolean = false;
 
   async login() {
+    // Verifica que el usuario no esté ya conectado
+    if (this.loggedIn) {
+      return;
+    }
+
     // Verifica que el usuario haya ingresado una contraseña
-   /*  if (!this.password) {
+    if (!this.password) {
       alert('Debes ingresar una contraseña');
       return;
     }
-    */
+
     // Verifica que el usuario exista en el almacenamiento
     const options: GetOptions = {
       key: 'user',
     };
-  
+
     const userStr = await Storage.get(options);
     if (userStr.value === null || userStr.value === '') {
       alert('El usuario no existe');
       return;
     }
-  
+
     // Obtiene el usuario del almacenamiento
-    const user = JSON.parse(userStr.value);
-  
+    const currentUser = JSON.parse(userStr.value);
+
     // Verifica que la contraseña sea correcta
-    /*if (user.password !== this.password) {
-      alert('La contraseña es incorrecta');
+    if (this.currentUser.user !== currentUser.user || this.password !== currentUser.password) {
+      alert('El usuario o la contraseña no son correctos');
       return;
     }
-   */
+
+    // Establece el usuario como conectado
+    this.loggedIn = true;
+
     // Redirige al usuario a la página principal
-    this.router.navigate(['/main'], { queryParams: { user: this.user } });
+    this.router.navigate(['/main'], { queryParams: { user: this.currentUser } });
   }
 
   ngOnInit() {
